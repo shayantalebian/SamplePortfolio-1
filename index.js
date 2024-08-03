95c1bf39c24aa73c35df88cffa73817b60aac323
Shayan Talebian <shayantalebianwork@gmail.com>
2024-08-03 12:18:28 +0330

    Fix some bugs in navbar

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 const menu = document.querySelector(".menu");
 const close = document.querySelector(".close");
 const nav = document.querySelector(".nav");
+menu.addEventListener("click", function () {
+  nav.classList.toggle("r-nav");
+});
+close.addEventListener("click", function () {
+  nav.classList.toggle("r-nav");
+});
+
 const li1 = document.querySelector(".js-close1");
 const li2 = document.querySelector(".js-close2");
 const li3 = document.querySelector(".js-close3");
 const li4 = document.querySelector(".js-close4");
 const li5 = document.querySelector(".js-close5");
 const li6 = document.querySelector(".js-close6");
-menu.addEventListener("click", function () {
-  nav.style.display = "flex";
-});
-close.addEventListener("click", function () {
-  nav.style.display = "none";
-});
+
 li1.addEventListener("click", function () {
-  nav.style.display = "none";
+  nav.classList.remove("r-nav");
 });
 li2.addEventListener("click", function () {
-  nav.style.display = "none";
+  nav.classList.remove("r-nav");
 });
 li3.addEventListener("click", function () {
-  nav.style.display = "none";
+  nav.classList.remove("r-nav");
 });
 li4.addEventListener("click", function () {
-  nav.style.display = "none";
+  nav.classList.remove("r-nav");
 });
 li5.addEventListener("click", function () {
-  nav.style.display = "none";
+  nav.classList.remove("r-nav");
 });
 li6.addEventListener("click", function () {
-  nav.style.display = "none";
+  nav.classList.remove("r-nav");
 });
